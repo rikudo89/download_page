77d48c16246f30ed230d695d9764fd71d14b3282
rikudo89 <35001929+rikudo89@users.noreply.github.com>
2019-07-09 05:57:18 +0700

    Update download.js

--- download.js
+++ download.js
@@ -131,7 +131,7 @@ function auto_safeconvert() {
         }
         if (a_to_vh == false) {
             var encryptedUrl = Base64.encode(a_to_vi);
-            a_to_ve[i].href = "https://download.filmindo89.com/movie/?url=" + encryptedUrl;
+            a_to_ve[i].href = "https://download.filmindo89.com/download.html?url=" + encryptedUrl;
             a_to_ve[i].rel = "nofollow";
             a_to_vb++;
             a_to_vc += i + ":::" + a_to_ve[i].href + "\n";
